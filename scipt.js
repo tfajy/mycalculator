ba9774967c693236e11f2ddbe4ca802e336527ab
fisolami fajuke <omotadefajuke@gmail.com>
2024-09-22 17:25:20 +0100

    done

---
+++ scipt.js
@@ -0,0 +1,53 @@
+let btnOne = document.getElementById("one");
+let display = document.getElementById("display");
+function one() {
+  display.value += "1";
+}
+function two() {
+  display.value += "2";
+}
+function three() {
+  display.value += "3";
+}
+function four() {
+  display.value += "4";
+}
+function five() {
+  display.value += "5";
+}
+function six() {
+  display.value += "6";
+}
+function seven() {
+  display.value += "7";
+}
+function eight() {
+  display.value += "8";
+}
+function nine() {
+  display.value += "9";
+}
+function zero() {
+  display.value += "0";
+}
+function divide() {
+  display.value += "/";
+}
+function subtract() {
+  display.value += "-";
+}
+function multiply() {
+  display.value += "*";
+}
+function point() {
+  display.value += ".";
+}
+function equal() {
+  display.value = eval(display.value);
+}
+function plus() {
+  display.value += "+";
+}
+function clearAll() {
+  display.value = " ";
+}
